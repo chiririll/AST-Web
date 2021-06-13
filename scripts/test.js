5239627c947b455e179c369fa6728d3bb23fe1f5
Kirill Chizhov <sstive39@gmail.com>
2021-06-13 16:58:18 +0300

    add regExp check

--- scripts/test.js
+++ scripts/test.js
@@ -3,10 +3,10 @@ function randInt(min, max) {
 }
 
 let q = 0;
-if (window.location.href.split('?')[1] == "rand")
-    q = randInt(0, questions.length);
-else if (!isNaN(Number(window.location.href.split('?')[1])))
+if (!isNaN(Number(window.location.href.split('?')[1])))
     q = Number(window.location.href.split('?')[1]);
+if (q >= questions.length)
+    q = 0;
 
 $(document).ready(setData);
 
@@ -14,11 +14,11 @@ $(document).ready(setData);
 /* Making test */
 function setData() {
     $("#qid").text(q+1);
-    $("#qcnt").text(questions.length+1);
+    $("#qcnt").text(questions.length);
 
     let progress = Math.floor(q/questions.length*100) + "%";
-    $("#greenBar").text(progress);
-    $("#greenBar").css('width', progress);
+    $("#greenBar").text(Math.floor(q/questions.length*100) + "%");
+    $("#greenBar").css('width', q/questions.length*100 + "%");
     
     $("#Q").text(questions[q]["task"]);
     $("#S").text(questions[q]["question"]);
@@ -46,32 +46,53 @@ function setSingleMany() {
         text = v.split(':')[1].trim();
         el_class = v.split(':')[0].trim() == '+' ? "right" : "wrong";
         $("#answers").append(
-            "<p class=\"" + el_class + "\"><input type=\"" + type + "\"" + (type == "radio" ? " onclick=\"check()\"" : "") + ">" + text + "</p>"
+            `<p class="${el_class}"><input type="${type}"` + (type == 'radio' ? ' onclick="check()"' : '') + `/>${text}</p>`
         );
     });
 }
 
 function setInput() {
-    $("#answers").append("<p class=\"\" id=\"varInp\"><input name=\"v1\" type=\"text\"></p>");
-    $("#answers").append("<p class=\"rAnswHide\">Right answers:</p><ul id=\"answersInp\" class=\"rAnswHide\"></ul>");
+    $("#answers").append('<p id="varInp"><input id="vInp" type="text"/></p>');
+    $("#answers").append('<p class="rAnswHide">Right answers:</p><ul id="answersInp" class="rAnswHide"></ul>');
     
     for (i = 0; i < questions[q]["vars"].length / 2; i++) {
-        $("#answersInp").append("<li>" + questions[q]["vars"][i].split(':')[1].trim() + "</li>");
+        $("#answersInp").append(`<li>${questions[q]["vars"][i].split(':')[1].trim()}</li>`);
     }
 }
 
 function setCompliance() {
     questions[q]["vars"].forEach(function(v) {
-        $("#answers").append("<p>" + v + "</p>");
+        $("#answers").append(`<p>${v}</p>`);
     });
 }
 /* =========== */
+
+/* Checking */
+function checkInput() {
+    let val = $("#vInp").val();
+    if (!val) return false;
+
+    let match = false;
+    questions[q]['vars'].forEach(function(v) {
+        v = v.split(':')[1].trim().replaceAll('*', '.?').replaceAll("#$#", '.*');
+        console.log(v);
+        try {
+            let re = new RegExp(v, 'i');
+            match = match || re.test(val);
+        } catch (err) {}
+    });
+    return match;
+}
+/* ======== */
 
 /* Buttons */
 function check() {
     $("input").each(function () {
         $(this).attr('disabled', true);
     });
+    
+    $("#vInp").css('background-color', checkInput() ? "yellowgreen" : "palevioletred")
+
     $(".right").each(function () {
         $(this).css('background-color', 'yellowgreen');
     });
